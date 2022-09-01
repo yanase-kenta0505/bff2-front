12b0f979464b773a9220173fc284dbb761cd0e1f
yanasekenta <ynsknt0505@gmail.com>
2022-09-01 16:52:06 +0900

    graphqlでBFFにqueryを投げる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: [
+    "@nuxt3-graphql/urql",
+  ],
+  urql: {
+    url: 'http://localhost:4000/graphql/'
+    // url: "https://rickandmortyapi.com/graphql/"
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <h3>hello world</h3>
+  <div v-if="fetching">
+    Loading...
+  </div>
+  <div v-else-if="error">
+    Oh no... {{error}}
+  </div>
+  <div v-else>
+    <ul v-if="data">
+      <li v-for="post in data.posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { useQuery } from '@urql/vue';
+
+export default {
+  setup() {
+    const result = useQuery({
+      query: `
+        {
+          posts {
+            id
+            title
+          }
+        }
+      `
+    });
+
+    console.log(result.data)
+    return {
+      fetching: result.fetching,
+      data: result.data,
+      error: result.error,
+    };
+    
+  }
+};
+</script>
